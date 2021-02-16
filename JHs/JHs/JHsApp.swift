ba2af63282d08fa4fc45d467731603fb2d342334
enfycius <hexk0131@gmail.com>
2021-02-16 14:15:20 +0900

    Resolve Issue #1

---
+++ JHs/JHs/JHsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JHsApp.swift
+//  JHs
+//
+//  Created by 김종혁 on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct JHsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JHs/JHsTests/JHsTests.swift
@@ -0,0 +1,33 @@
+//
+//  JHsTests.swift
+//  JHsTests
+//
+//  Created by 김종혁 on 2021/02/16.
+//
+
+import XCTest
+@testable import JHs
+
+class JHsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
