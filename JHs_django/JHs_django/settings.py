dcb89a9e8bfeab697307320e471cf613e5c4c63f
김종혁 <hexk0131@gimjonghyeog-ui-MacBookPro.local>
2021-08-18 14:13:00 +0900

    Fix feature/issue-#6-settings

--- JHs_django/JHs_django/settings.py
+++ JHs_django/JHs_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +74,7 @@ WSGI_APPLICATION = 'JHs_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
