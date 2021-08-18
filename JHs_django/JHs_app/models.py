a7d3dfed91f1b79336af6324707af3563dfbf7a9
김종혁 <hexk0131@gimjonghyeog-ui-MacBookPro.local>
2021-08-18 14:28:31 +0900

    Fix feature/issue-#10-model

--- JHs_django/JHs_app/models.py
+++ JHs_django/JHs_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField("Email", max_length=30, blank = True, null = True)
+    password = models.CharField("Password", max_length=20, blank = True, null = True)
